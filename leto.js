fe705f38664745a94766b36026763d89208e813e
ZECTBynmo <michael.vegeto@gmail.com>
2013-06-22 22:52:34 -0400

    Add the crawl step to the main leto script

--- leto.js
+++ leto.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 var Spawner = require("./spawner").spawner,
+	Crawler = require("./crawler").crawler,
 	needle = require("needle"),
 	spawner = new Spawner(),
-	urls = require("./urls.json");
+	crawler = new Crawler(),
+	urls = require("./urls.json")
+
+require('json5/lib/require');
 
 // Enum our arg indices for code clarity
 var ARG_PROCESS = 0,	// node
-	ARG_SCRIPT = 1,		// spawn
-	ARG_ARG1 = 2,		// new/publish
+	ARG_SCRIPT = 1,		// leto
+	ARG_ARG1 = 2,		// spawn/crawl/publish
 	ARG_ARG2 = 3;		// ...
 
 if( process.argv.length < 3 ) {
@@ -24,6 +28,10 @@ case "publish":
  	publishTemplate( process.argv[ARG_ARG2] );
  	break;
 
+case "crawl": 	
+ 	crawlTemplate( process.argv[ARG_ARG2] );
+ 	break;
+
 default:
   	console.log( "Argument " + process.argv[ARG_ARG1] + " is not recognized" );
 }
@@ -38,13 +46,21 @@ function publishTemplate( source ) {
 	console.log( "Publishing template project in " + source );
 
 	var template = {},
-		setupJSON = require( source + "/template_setup.json" );
+		setupJSON = require( source + "/leto.json5" );
 
-	template.setup = setupJSON;
+	try {
+		var templateParams = require( source + "/leto_params.json" );
+	} catch( err ) {
+		console.log( "Couldn't find " + source + "/leto_params.json, run 'leto crawl' on the project to generate it" );
+		return;
+	}
+
+	template = setupJSON;
+	template.__params = templateParams;
 
 	console.log( template );
 
-	needle.post( urls.REMOTE_URL + "/templates", template, function(){} );
+	needle.post( urls.LOCAL_URL + "/templates", template, function(error){if(error!=undefined)console.log(error);} );
 }
 
 
@@ -68,5 +84,23 @@ function spawnProject( contentsHash ) {
 		spawner.spawn( dest, body.setup, options, body.contents, function() {
 			console.log( "Finished spawning" );
 		});
+	});
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// Crawls over a template folder and expose all of the template params
+function crawlTemplate( source ) {
+	console.log( "Crawling template folder" );
+
+	console.log( "Add descriptions for template params inside of leto_params.json to add comments/tooltips in the registry" );
+
+	// Default the source to the current working directory
+	source = source || process.cwd();
+
+	var setupJSON = require( source + "/leto.json5" );
+
+	crawler.crawl( source, setupJSON, function() {
+		console.log( "Finished crawling" );
 	});
 }
